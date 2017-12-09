c3499439dd56779ad338074c98eb70968ae10c1c
Dimitris <dkout@mit.edu>
2017-12-09 15:30:07 -0500

    fw pass test

---
+++ alexnet_bn_fwpass.py
@@ -0,0 +1,181 @@
+import os, datetime
+import numpy as np
+import tensorflow as tf
+import sys
+import datetime
+from tensorflow.contrib.layers.python.layers import batch_norm
+from TrainDataLoader import *
+
+#Restore parameters
+
+restore_path = "4thrun_kout/alexnet_bn-30000"
+# Dataset Parameters
+batch_size = 1
+load_size = 256 
+fine_size = 224
+c = 3
+data_mean = np.asarray([0.45834960097,0.44674252445,0.41352266842])
+
+# Training Parameters
+learning_rate = 0.0005
+dropout = 0.5 # Dropout, probability to keep units
+training_iters = 50000
+step_display = 1
+step_save = 5000
+path_save = './alexnet_bn'
+endtime = datetime.datetime.now()+datetime.timedelta(hours=17)
+start_from = ''
+
+def batch_norm_layer(x, train_phase, scope_bn):
+    return batch_norm(x, decay=0.9, center=True, scale=True,
+    updates_collections=None,
+    is_training=train_phase,
+    reuse=None,
+    trainable=True,
+    scope=scope_bn)
+    
+def alexnet(x, keep_dropout, train_phase):
+    weights = {
+        'wc1': tf.Variable(tf.random_normal([11, 11, 3, 96], stddev=np.sqrt(2./(11*11*3)))),
+        'wc2': tf.Variable(tf.random_normal([5, 5, 96, 256], stddev=np.sqrt(2./(5*5*96)))),
+        'wc3': tf.Variable(tf.random_normal([3, 3, 256, 384], stddev=np.sqrt(2./(3*3*256)))),
+        'wc4': tf.Variable(tf.random_normal([3, 3, 384, 256], stddev=np.sqrt(2./(3*3*384)))),
+        'wc5': tf.Variable(tf.random_normal([3, 3, 256, 256], stddev=np.sqrt(2./(3*3*256)))),
+
+        'wf6': tf.Variable(tf.random_normal([7*7*256, 4096], stddev=np.sqrt(2./(7*7*256)))),
+        'wf7': tf.Variable(tf.random_normal([4096, 4096], stddev=np.sqrt(2./4096))),
+        'wo': tf.Variable(tf.random_normal([4096, 100], stddev=np.sqrt(2./4096)))
+    }
+
+    biases = {
+        'bo': tf.Variable(tf.ones(100))
+    }
+
+    # Conv + ReLU + Pool, 224->55->27
+    conv1 = tf.nn.conv2d(x, weights['wc1'], strides=[1, 4, 4, 1], padding='SAME')
+    conv1 = batch_norm_layer(conv1, train_phase, 'bn1')
+    conv1 = tf.nn.relu(conv1)
+    pool1 = tf.nn.max_pool(conv1, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='SAME')
+
+    # Conv + ReLU  + Pool, 27-> 13
+    conv2 = tf.nn.conv2d(pool1, weights['wc2'], strides=[1, 1, 1, 1], padding='SAME')
+    conv2 = batch_norm_layer(conv2, train_phase, 'bn2')
+    conv2 = tf.nn.relu(conv2)
+    pool2 = tf.nn.max_pool(conv2, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='SAME')
+
+    # Conv + ReLU, 13-> 13
+    conv3 = tf.nn.conv2d(pool2, weights['wc3'], strides=[1, 1, 1, 1], padding='SAME')
+    conv3 = batch_norm_layer(conv3, train_phase, 'bn3')
+    conv3 = tf.nn.relu(conv3)
+
+    # Conv + ReLU, 13-> 13
+    conv4 = tf.nn.conv2d(conv3, weights['wc4'], strides=[1, 1, 1, 1], padding='SAME')
+    conv4 = batch_norm_layer(conv4, train_phase, 'bn4')
+    conv4 = tf.nn.relu(conv4)
+
+    # Conv + ReLU + Pool, 13->6
+    conv5 = tf.nn.conv2d(conv4, weights['wc5'], strides=[1, 1, 1, 1], padding='SAME')
+    conv5 = batch_norm_layer(conv5, train_phase, 'bn5')
+    conv5 = tf.nn.relu(conv5)
+    pool5 = tf.nn.max_pool(conv5, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='SAME')
+
+    # FC + ReLU + Dropout
+    fc6 = tf.reshape(pool5, [-1, weights['wf6'].get_shape().as_list()[0]])
+    fc6 = tf.matmul(fc6, weights['wf6'])
+    fc6 = batch_norm_layer(fc6, train_phase, 'bn6')
+    fc6 = tf.nn.relu(fc6)
+    fc6 = tf.nn.dropout(fc6, keep_dropout)
+    
+    # FC + ReLU + Dropout
+    fc7 = tf.matmul(fc6, weights['wf7'])
+    fc7 = batch_norm_layer(fc7, train_phase, 'bn7')
+    fc7 = tf.nn.relu(fc7)
+    fc7 = tf.nn.dropout(fc7, keep_dropout)
+
+    # Output FC
+    out = tf.add(tf.matmul(fc7, weights['wo']), biases['bo'])
+    
+    return out
+
+# Construct dataloader
+opt_data_train = {
+    #'data_h5': 'miniplaces_256_train.h5',
+    'data_root': '../../../data/images/train',   # MODIFY PATH ACCORDINGLY
+    'data_list': '../../../data/train.txt', # MODIFY PATH ACCORDINGLY
+    'load_size': load_size,
+    'fine_size': fine_size,
+    'data_mean': data_mean,
+    'randomize': True
+    }
+opt_data_val = {
+    #'data_h5': 'miniplaces_256_val.h5',
+    'data_root': '../../../data/images/val',   # MODIFY PATH ACCORDINGLY
+    'data_list': '../../../data/val.txt',   # MODIFY PATH ACCORDINGLY
+    'load_size': load_size,
+    'fine_size': fine_size,
+    'data_mean': data_mean,
+    'randomize': False
+    }
+
+opt_data_test = {
+    #'data_h5': 'miniplaces_256_val.h5',
+    'data_root': '../letters',   # MODIFY PATH ACCORDINGLY
+    'data_list': '../letters/test.txt',   # MODIFY PATH ACCORDINGLY
+    'load_size': load_size,
+    'fine_size': fine_size,
+    'data_mean': data_mean,
+    'randomize': False
+    }
+
+loader_train = DataLoaderDisk(**opt_data_train)
+loader_val = DataLoaderDisk(**opt_data_val)
+loader_test = DataLoaderDisk(**opt_data_test)
+#loader_train = DataLoaderH5(**opt_data_train)
+#loader_val = DataLoaderH5(**opt_data_val)
+
+# tf Graph input
+x = tf.placeholder(tf.float32, [None, fine_size, fine_size, c])
+#y = tf.placeholder(tf.int64, None)
+keep_dropout = tf.placeholder(tf.float32)
+train_phase = tf.placeholder(tf.bool)
+
+# Construct model
+logits = alexnet(x, keep_dropout, train_phase)
+
+probabilities = tf.nn.softmax(logits)
+
+# Define loss and optimizer
+#loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y, logits=logits))
+
+# define summary writer
+#writer = tf.train.SummaryWriter('.', graph=tf.get_default_graph())
+
+# Launch the graph
+
+with tf.Session() as sess:
+    # Initialization
+    saver=tf.train.Saver()
+    saver.restore(sess,restore_path) 
+    # Evaluate on the whole validation set
+    print('Test on the whole test set...')
+    num_batch = loader_test.size()//batch_size
+    acc1_total = 0.
+    acc5_total = 0.
+    loader_test.reset()
+    for i in range(num_batch):
+        images_batch, name_batch = loader_test.next_batch(batch_size)    
+        output = sess.run(probabilities, feed_dict={x: images_batch, train_phase: False, keep_dropout: 1.})#, dropout: 1.})
+        output = output[0, 0:]
+        sorted_inds = [i[0] for i in sorted(enumerate(-output), key=lambda x:x[1])]
+        print( str(name_batch[:]) + " ", end='')
+        for j in range(5):
+            index = sorted_inds[j]
+            print(str(index) + ' ', end='')
+        print("")
+        sys.stdout.flush()
+
+    print('***END***')
+
+    sys.stdout.flush()
+
+    
